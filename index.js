ebe68e85f39a7aa71c223c6fc980899c617a9fcc
singhzrepo <contactwithsid@gmail.com>
2023-08-17 20:01:44 +0530

    Added setup for socket io

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+    
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('message', data => {
+        console.log("Message from frontend", data)
+        io.emit('message_back', data)
+    })
+
+  });
+
+server.listen(8000, () => {
+  console.log('listening on *:8000');
+});
